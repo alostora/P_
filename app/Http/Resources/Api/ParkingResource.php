5df10dc99e4a199ed504191738b89b5cd29d92cc
alostora <dolsika5762@gmail.com>
2024-07-11 19:34:08 +0300

    add CR_serial to print text inside parking resource

--- app/Http/Resources/Api/ParkingResource.php
+++ app/Http/Resources/Api/ParkingResource.php
@@ -17,7 +17,7 @@ class ParkingResource extends JsonResource
      */
     public function toArray($request)
     {
-        
+
 
         $resource =  [
 
@@ -52,40 +52,39 @@ class ParkingResource extends JsonResource
         ];
 
 
-        $id = PHP_EOL .'ivoice number : ' . $this->id . PHP_EOL ;
+        $id = PHP_EOL . 'ivoice number : ' . $this->id . PHP_EOL;
 
-        $code = PHP_EOL .'parking code : ' . $this->code . PHP_EOL ;
+        $code = PHP_EOL . 'parking code : ' . $this->code . PHP_EOL;
 
         $company_name = 'Valet & Parking Management' . PHP_EOL;
 
-        $tax_registar = 'VAT No. : 311279813600003'. PHP_EOL;
+        $CRserial = 'C.R : 4030264321' . PHP_EOL;
 
-        $starts_at = 'Entry Date : ' . date('Y-m-d H:i',strtotime($this->starts_at)). PHP_EOL;
+        $tax_registar = 'VAT No. : 311279813600003' . PHP_EOL;
 
-        $ends_at = 'Exit Date : ' . date('Y-m-d H:i',strtotime($this->ends_at)). PHP_EOL;
+        $starts_at = 'Entry Date : ' . date('Y-m-d H:i', strtotime($this->starts_at)) . PHP_EOL;
+
+        $ends_at = 'Exit Date : ' . date('Y-m-d H:i', strtotime($this->ends_at)) . PHP_EOL;
 
         $cost = 'Cost : ' . $this->cost . " SAR" . PHP_EOL;
 
         $total = 'Total : ' . $this->cost . " SAR" . PHP_EOL;
 
-        $web = 'https://vpmsystems.com'. PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL;
+        $web = 'https://vpmsystems.com' . PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL . PHP_EOL;
 
-        if($this->ends_at != null){
+        if ($this->ends_at != null) {
 
-            
+
             $invoice = PHP_EOL . 'Invoice No.: : ' . $this->id . PHP_EOL;
 
-            $code = 'parking code : ' . $this->code . PHP_EOL ;
+            $code = 'parking code : ' . $this->code . PHP_EOL;
 
-            $resource['print_text'] = $invoice . $id . $company_name . $tax_registar . $starts_at . $ends_at  . $cost . $total . $web;
+            $resource['print_text'] = $invoice . $id . $company_name . $CRserial . $tax_registar . $starts_at . $ends_at  . $cost . $total . $web;
+        } else {
 
-        }else{
-
-            $resource['print_text'] = $id . $company_name . $tax_registar . $starts_at . $web;
-
+            $resource['print_text'] = $id . $company_name . $CRserial . $tax_registar . $starts_at . $web;
         }
 
         return $resource;
-
     }
 }
